5816bb7c665355b017c1db94241d7aa0254f8387
Andres Molina <heikel@oktana.io>
2020-06-25 12:01:58 -0300

    Some fixes for course repo and controller

--- app/repositories/CourseRepository.scala
+++ app/repositories/CourseRepository.scala
@@ -1,7 +1,7 @@
 package repositories
 
 import models.Course
-import play.api.libs.json.Json
+import play.api.libs.json.{JsObject, Json}
 import play.api.mvc.{AbstractController, ControllerComponents}
 import play.modules.reactivemongo.{
   MongoController,
@@ -9,11 +9,13 @@ import play.modules.reactivemongo.{
   ReactiveMongoComponents
 }
 import javax.inject.Inject
+
 import scala.concurrent.{ExecutionContext, Future}
 import reactivemongo.bson.BSONDocument
 import reactivemongo.play.json._
 import collection._
 import reactivemongo.api.Cursor
+import reactivemongo.api.Cursor.WithOps
 
 class CourseRepository @Inject()(val components: ControllerComponents,
                                  val reactiveMongoApi: ReactiveMongoApi)
@@ -32,62 +34,38 @@ class CourseRepository @Inject()(val components: ControllerComponents,
   def createAll(courses: List[Course]) =
     collection.flatMap(_.insert.many(courses))
 
-  def find() = {
-    val cursor: Future[Cursor[Course]] = collection.map {
-      _.find(BSONDocument()).cursor[Course]()
-    }
+//  def find() = {
+//    val cursor: Future[Cursor[Course]] = collection.map {
+//      _.find(BSONDocument()).cursor[Course]()
+//    }
+//
+//    val futureCoursesList =
+//      cursor.flatMap(_.collect[List](-1, Cursor.FailOnError[List[Course]]()))
+//    futureCoursesList
+//  }
+//
+//  def findId(id: Long) = {
+//    val cursor: Future[Cursor[Course]] = collection.map {
+//      _.find(Json.obj("_id" -> id)).cursor[Course]()
+//    }
+//
+//    val futureCoursesList: Future[List[Course]] =
+//      cursor.flatMap(_.collect[List](-1, Cursor.FailOnError[List[Course]]()))
+//    futureCoursesList
+//  }
 
-    val futureCoursesList =
-      cursor.flatMap(_.collect[List](-1, Cursor.FailOnError[List[Course]]()))
-    futureCoursesList
-  }
-
-  def findId(id: Long) = {
-    val cursor: Future[Cursor[Course]] = collection.map {
-      _.find(Json.obj("_id" -> id)).cursor[Course]()
-    }
-
-    val futureCoursesList: Future[List[Course]] =
-      cursor.flatMap(_.collect[List](-1, Cursor.FailOnError[List[Course]]()))
-    futureCoursesList
-  }
-
-  //practicing..
-
-  abstract class findSomething
-
-  case class afind() extends findSomething
-
-  case class afindById(id: Long) extends findSomething
-
-  def find3(find: findSomething) = {
-    find match {
-      case afind() =>
-        val cursor: Future[Cursor[Course]] = collection.map {
+  def find(idOpt: Option[Long]) = {
+    val a: Future[WithOps[Course]] = idOpt match {
+      case Some(x) =>
+        collection.map {
+          _.find(Json.obj("_id" -> x)).cursor[Course]()
+        }
+      case _ =>
+        collection.map {
           _.find(BSONDocument()).cursor[Course]()
         }
-
-        val futureCoursesList =
-          cursor.flatMap(
-            _.collect[List](-1, Cursor.FailOnError[List[Course]]())
-          )
-        futureCoursesList
-
-      case afindById(id) =>
-        val cursor: Future[Cursor[Course]] = collection.map {
-          _.find(Json.obj("_id" -> id)).cursor[Course]()
-        }
-
-        val futureCoursesList =
-          cursor.flatMap(
-            _.collect[List](-1, Cursor.FailOnError[List[Course]]())
-          )
-        futureCoursesList
     }
+
+    a.flatMap(_.collect[List](-1, Cursor.FailOnError[List[Course]]()))
   }
-
-  println(find3(afind()))
-  println(find3(afindById(3)))
-
-  //end of practicing
 }
